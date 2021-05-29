519533051519cb17be5972a4967bc5cf5d09c02f
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2021-05-29 19:11:55 +0200

    create components + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './content/contact/contact.component';
+import { HomeComponent } from './content/home/home.component';
+import { PresentingComponent } from './content/presenting/presenting.component';
+import { ServicesComponent } from './content/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'presenting', component: PresentingComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './content/home/home.component';
+import { PresentingComponent } from './content/presenting/presenting.component';
+import { ContactComponent } from './content/contact/contact.component';
+import { ServicesComponent } from './content/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PresentingComponent,
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
